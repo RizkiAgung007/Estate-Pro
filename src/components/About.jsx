1991990807eafde9fa40d16a72ed41082705770e
unknown <client689k@gmail.com>
2025-03-05 16:18:29 +0700

    update about dan tambah project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import { assets } from "../assets/assets"
 
 const About = () => {
   return (
     <div
       id="About"
       className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden">
-      About
+      <h1 className="text-2xl sm:text-4xl font-medium mb-2">About <span className="text-blue-500 underline underline-offset-6 decoration-1 font-light">
+        Our Website</span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8">Our website is a platform that allows users to search and sell the property they are looking for.</p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        <img src={ assets.brand_img } className="w-full sm:w-1/2 max-w-lg" alt=""/>
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-500">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-600">10+</p>
+              <p>Years of Excellence</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-600">12+</p>
+              <p>Projects Completed</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-600">18+</p>
+              <p>Mn.Sq.Ft.Delivered</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-600">26+</p>
+              <p>Ongoing Projects</p>
+            </div>
+          </div>
+          <p className="text-base mt-10 max-w-lg text-justify">
+            Our website is an innovative platform designed to make it easier for users 
+            to search and sell different types of properties according to their needs. 
+            With its intuitive interface and advanced search features, users can easily find 
+            their dream property based on location, price, and other criteria. In addition, 
+            we also provide services for property owners who want to sell or rent their assets with a wider reach. 
+            We are committed to creating an efficient, safe and convenient property search experience for all users.
+          </p>
+          <div className="mt-5">
+            <button className="border border-blue-500 hover:bg-blue-500 hover:text-white px-8 py-2 rounded duration-75 ease-in cursor-pointer">Learn More</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Projects.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import { assets, projectsData } from '../assets/assets'
+
+const Projects = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [cartShow, setCardShow] = useState(1);
+
+    // Menggunakan useEffect untuk menyesuaikan jumlah kartu yang ditampilkan berdasarkan ukuran layar
+    useEffect(() => {
+        const updateCartShow = () => {
+            if (window.innerWidth >= 1024) {
+                setCardShow(projectsData.length);
+            } else {
+                setCardShow(1);
+            }
+        };
+        updateCartShow();
+        
+        // Tambah dan hapus event listener untuk menangani perubahan ukuran layar
+        window.addEventListener('resize', updateCartShow);
+        return () => window.removeEventListener('resize', updateCartShow);
+    }, []);
+
+    const nextIndex = () => {
+        setCurrentIndex((prevIndex) => 
+            (prevIndex + 1) % projectsData.length // Jika sudah di indeks terakhir, kembali ke awal
+        );
+    };
+
+    const prevIndex = () => {
+        setCurrentIndex((prevIndex) => 
+            prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1 // Jika di indeks pertama, kembali ke indeks terakhir
+        );
+    };
+
+
+  return (
+    <div id='Project' className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden'>
+        <h1 className='text-2xl sm:text-4xl font-medium mb-2 text-center'>Projects <span className="text-blue-500 underline underline-offset-6 decoration-1 font-light">Completed</span></h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Building Legacies-Explore Our Portofolio</p>
+
+        {/* Slider Button */}
+        <div className='flex justify-end items-center mb-8'>
+            <button onClick={ prevIndex } className='p-3 bg-gray-300 rounded mr-2 cursor-pointer'>
+                <img src={ assets.left_arrow } alt='Prev' />
+            </button>
+            <button onClick={ nextIndex } className='p-3 bg-gray-300 rounded mr-2 cursor-pointer'>
+                <img src={ assets.right_arrow } alt='Prev' />
+            </button>
+        </div>
+
+        {/* Project slider */}
+        <div className='overflow-hidden'>
+            <div className='flex gap-8 transition-transform duration-200 ease-in-out'
+            style={{transform: `translateX(-${(currentIndex * 100) / cartShow}%)`}}>
+                {projectsData.map((project, index) => (
+                    <div key={ index } className='relative flex-shrink-0 w-full sm:w-1/4'>
+                        <img src={ project.image } alt={ project.title } className='w-full h-auto mb-14' />
+                        <div className='absolute right-0 left-0 bottom-5 flex justify-center'>
+                            <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                                <h2 className='text-xl font-semibold text-gray-500'>
+                                    { project.title }
+                                </h2>
+                                <p className='text-gray-500 text-sm'>
+                                    { project.price} <span> </span> { project.location }
+                                </p>
+                            </div>
+                        </div>    
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
